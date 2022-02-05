f83e6a519294153b3ebc0f13c072fafad1874bd0
roggc <roggc9@gmail.com>
2022-02-05 19:53:57 +0100

    set menu

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { useWindowDimensions } from '../../hooks'
 
 interface IScreenProps extends React.HTMLAttributes<HTMLDivElement>{
     header:React.ReactNode
     footer:React.ReactNode
+    menu:React.ReactNode
 }
 
-export const Screen:React.FC<IScreenProps>=({header,footer,children,...props})=>{
+export const Screen:React.FC<IScreenProps>=({header,footer,menu,children,...props})=>{
     const {windowWidth,windowHeight}=useWindowDimensions()
+    const [isHidden,setIsHidden]=useState(true)
     return <ScreenBox width={windowWidth} height={windowHeight} {...props}>
+        <MenuWrapper onClick={()=>setIsHidden(prev=>!prev)}>{menu}</MenuWrapper>
+        <MenuScreen height={windowHeight} width={225} isHidden={isHidden}></MenuScreen>
         {header}
         {children}
         {footer}
@@ -25,4 +29,26 @@ display:flex;
 flex-direction:column;
 justify-content:space-between;
 align-items:center;
+position:relative;
+overflow:hidden;
+`
+
+const MenuWrapper=styled.div`
+position:absolute;
+top:10px;
+left:10px;
+cursor:pointer;
+z-index:2;
+`
+
+const MenuScreen=styled.div<{width:number;height:number;isHidden:boolean}>`
+position:absolute;
+top:0;
+left:${({isHidden,width})=>`${isHidden?`${-(width+1)}px`:'0'}`};
+width:${({width})=>width}px;
+height:${({height})=>height}px;
+background-color:${({theme})=>theme.backgroundColor};
+z-index:1;
+transition:left 1s;
+box-sizing:border-box;
 `
--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -7,6 +7,8 @@ import { ManProgramming } from '../ManProgramming'
 import { SoccerField } from '../SoccerField'
 import { Screen } from '../Screen'
 import { useGetWidthsAndHeights } from '../../hooks'
+import {Menu} from '@styled-icons/evaicons-solid/Menu'
+import {Cross} from '@styled-icons/entypo/Cross'
 
 const getLotties = () => [
   { name: 'manWalking', Lottie: ManWalking,footer:<Footer>walking</Footer> },
@@ -28,6 +30,7 @@ export const App = () => {
   const [isActivateChange, setIsActivateChange] = useState(false)
   const [lotties, setLotties] = useState(getLotties())
   const { widths, heights, refs } = useGetWidthsAndHeights(isActivateChange,setIsActivateChange,lotties.length)
+  const [isMenuOpen,setIsMenuOpen]=useState(false)
 
   const reorderLotties = () => {
     setLotties((prev) => {
@@ -40,7 +43,7 @@ export const App = () => {
 
   return (
     <ThemeProvider theme={myTheme}>
-      <Screen header={<Header>Some things I like</Header>} footer={lotties[lotties.length-1].footer}>
+      <Screen header={<Header>Some things I like</Header>} footer={lotties[lotties.length-1].footer} menu={isMenuOpen?<Icon as={Cross} onClick={()=>setIsMenuOpen(false)} />:<Icon as={Menu} onClick={()=>setIsMenuOpen(true)} />}>
         <CardsWrapper>
           {lotties.map((lottie, index, array) => (
             <Card
@@ -74,4 +77,8 @@ const Footer=styled.div`
 margin:20px;
 font-size:1.2em;
 font-weight:bold;
+`
+const Icon=styled.svg`
+width:35px;
+font-weight:700;
 `
